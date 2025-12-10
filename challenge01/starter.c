bb91861abf488a99d7aadf4e3fa3e5e812432024
Deivd730 <davidcastill2005@gmail.com>
2025-12-10 15:50:37 +0100

    first commit

--- challenge01/starter.c
+++ challenge01/starter.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 
-// Declaración de la función a implementar
+// 1. Declaración o prototipo de la función
 int suma(int a, int b);
 
 int main() {
-    // Ejemplo de uso rápido
-    printf("Resultado: %d\n", suma(2, 3));
+    int num1 = 10;
+    int num2 = 25;
+    
+    // Llamada a la función
+    int total = suma(num1, num2);
+    
+    printf("La suma de %d y %d es: %d\n", num1, num2, total);
+    
     return 0;
+}
+
+// 2. Definición de la lógica de la función
+int suma(int a, int b) {
+    return a + b;
 }
